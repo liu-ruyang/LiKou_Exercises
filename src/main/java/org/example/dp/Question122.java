f72c1dd23374e416d353f4f0a1459d33d54df872
lry <2920255877@qq.com>
2024-09-02 09:23:03 +0800

    2024/08/29

---
+++ src/main/java/org/example/dp/Question122.java
@@ -0,0 +1,19 @@
+package org.example.dp;
+
+public class Question122 {
+    public int maxProfit(int[] prices) {
+        int[][] dp = new int[prices.length][2];
+
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+        // dp[i][0]代表第i天持有股票的最大收益
+        // dp[i][1]代表第i天不持有股票的最大收益
+        for (int i = 1; i < prices.length; i++) {
+            // dp[i][0] = Math.max(dp[i - 1][0], -prices[i]);   // 121
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]);    // 注意这里是和121. 买卖股票的最佳时机唯一不同的地方。
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+        }
+
+        return dp[prices.length - 1][1];
+    }
+}
