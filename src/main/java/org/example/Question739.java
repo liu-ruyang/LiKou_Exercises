a575a8dab793849505fc3637bb74c0c5ee52644b
lry <2920255877@qq.com>
2024-09-03 10:43:08 +0800

    2024/9/03

---
+++ src/main/java/org/example/Question739.java
@@ -0,0 +1,52 @@
+package org.example;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Question739 {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Deque<Integer> stack = new LinkedList<Integer>();
+        int[] res = new int[temperatures.length];
+
+        for (int i = 0; i < temperatures.length; i++) {
+            if (stack.isEmpty()) {
+                stack.push(i);
+            } else {
+                while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                    Integer pop = stack.pop();
+                    res[pop] = i - pop;
+                }
+                stack.push(i);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            Integer pop = stack.pop();
+            res[pop] = 0;
+        }
+        return res;
+    }
+
+    public int[] dailyTemperatures2(int[] temperatures) {
+        Deque<Integer> stack = new LinkedList<Integer>();
+        int[] res = new int[temperatures.length];
+
+        for (int i = 0; i < temperatures.length; i++) {
+            if (stack.isEmpty()) {
+                stack.push(i);
+            } else {
+                while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                    Integer pop = stack.pop();
+                    res[pop] = i - pop;
+                }
+                stack.push(i);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            Integer pop = stack.pop();
+            res[pop] = 0;
+        }
+        return res;
+    }
+}
---
+++ src/main/java/org/example/dp/Question714.java
@@ -0,0 +1,20 @@
+package org.example.dp;
+
+public class Question714 {
+    public int maxProfit(int[] prices, int fee) {
+        int[][] dp = new int[prices.length][2];
+
+        // 0持有股票
+        // 1不持有股票
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i] - fee);
+        }
+
+        return dp[prices.length - 1][1];
+
+    }
+}
