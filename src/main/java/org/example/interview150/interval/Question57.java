dd78f736834be210ab33b9d2666d1b62255b466b
lry <2920255877@qq.com>
2024-10-13 14:12:03 +0800

    2024/10/13

--- src/main/java/org/example/interview150/interval/Question57.java
+++ src/main/java/org/example/interview150/interval/Question57.java
@@ -1,60 +1,40 @@
 package org.example.interview150.interval;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Question57 {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-//        Arrays.sort(intervals, (a, b) -> {
-//            return a[0] - b[0];
-//        });
-        if (intervals.length == 0) {
-            return new int[][]{newInterval};
+
+        ArrayList<int[]> nums = new ArrayList<>();
+        for (int[] interval : intervals) {
+            nums.add(interval);
         }
+        nums.add(newInterval);
+        int[][] array = nums.toArray(new int[nums.size()][]);
+
+
+        Arrays.sort(array, (a, b) -> {
+            return a[0] - b[0];
+        });
 
         ArrayList<int[]> res = new ArrayList<>();
 
-//        int left = intervals[0][0];
-//        int right = intervals[0][1];
-        int left = 0;
-        for (; left < intervals.length; left++) {
-            if (intervals[left][1] < newInterval[0]) {
-                res.add(intervals[left]);
-            } else
-                break;
-        }
-
-        if (intervals[left][0] > newInterval[1]) {
-            res.add(newInterval);
-        } else if (intervals[left][0] == newInterval[1]) {
-            res.add(new int[]{newInterval[0], intervals[left][1]});
-            left++;
-        } else if (intervals[left][1] >= newInterval[1]) {
-            int min = Integer.min(newInterval[0], intervals[left][0]);
-            res.add(new int[]{min, intervals[left][1]});
-            left++;
-        } else if (intervals[left][1] < newInterval[1]) {
-            int min = Integer.min(newInterval[0], intervals[left][0]);
-            for (; left < intervals.length; left++) {
-                if (intervals[left][0] > newInterval[1]) {
-                    res.add(new int[]{min, newInterval[1]});
-                    break;
-                } else if (intervals[left][0] <= newInterval[1] && intervals[left][1] >= newInterval[1]) {
-                    res.add(new int[]{min, intervals[left][1]});
-                    left++;
-                    break;
+        int left = array[0][0];
+        int right = array[0][1];
+        for (int i = 1; i < array.length; i++) {
+            if (right < array[i][1]) {
+                if (right >= array[i][0]) {
+                    right = array[i][1];
+                } else {
+                    res.add(new int[]{left, right});
+                    left = array[i][0];
+                    right = array[i][1];
                 }
             }
         }
-
-        for (; left < intervals.length; left++) {
-            res.add(intervals[left]);
-        }
-
-
-//        res.add(new int[]{left, right});
+        res.add(new int[]{left, right});
 
         return res.toArray(new int[res.size()][]);
-//        return (int[][]) res.stream().toArray();
-
     }
 }
