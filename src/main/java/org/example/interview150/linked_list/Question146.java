8e8da98de3aeba0918d59863e0e601ed61b06912
liu-ruyang <2920255877@qq.com>
2024-10-21 09:20:47 +0800

    pull最新，合并分支

--- src/main/java/org/example/interview150/linked_list/Question146.java
+++ src/main/java/org/example/interview150/linked_list/Question146.java
@@ -120,11 +120,4 @@ public class Question146 {
             ListNode next;
         }
     }
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
 }
