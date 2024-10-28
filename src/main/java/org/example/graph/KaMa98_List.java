39af1a0f240756544a769de6a545148bbd58e303
lry <2920255877@qq.com>
2024-10-28 16:53:46 +0800

    2024/10/24

--- src/main/java/org/example/graph/KaMa98_List.java
+++ src/main/java/org/example/graph/KaMa98_List.java
@@ -4,6 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+// void dfs(参数) {
+//     if (终止条件) {
+//         存放结果;
+//         return;
+//     }
+//     for (选择:
+//          本节点所连接的其他节点) {
+//         处理节点;
+//         dfs(图，选择的节点);   // 递归
+//         回溯，撤销处理结果;
+//     }
+// }
+
 public class KaMa98_List {
     static List<List<Integer>> result;
     static List<Integer> path;
@@ -17,14 +30,48 @@ public class KaMa98_List {
         int m = scanner.nextInt();
 
         // 邻接表法
+        // 初始化邻接表
         List<List<Integer>> graph = new ArrayList<>(n + 1);
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        // 读取输入构建邻接表
         for (int i = 0; i < m; i++) {
-            
+            graph.get(scanner.nextInt()).add(scanner.nextInt());
+        }
+
+        // 开始dfs
+        path.add(1);
+        dfs(graph, 1, n);
+
+        // 考虑特殊结果
+        if (result.size() == 0) {
+            System.out.println(-1);
+        }
+        // 打印结果
+        for (List<Integer> integers : result) {
+            for (int i = 0; i < integers.size(); i++) {
+                if (i == integers.size() - 1) {
+                    System.out.println(integers.get(i));
+                } else {
+                    System.out.print(integers.get(i) + " ");
+                }
+            }
         }
 
     }
 
-    public static void dfs() {
-
+    public static void dfs(List<List<Integer>> graph, int x, int n) {
+        if (x == n) {
+//            path.add(x);
+            result.add(new ArrayList<>(path));
+//            path.remove(path.size() - 1);
+        } else {
+            for (int i = 0; i < graph.get(x).size(); i++) {
+                path.add(graph.get(x).get(i));
+                dfs(graph, graph.get(x).get(i), n);
+                path.remove(path.size() - 1);
+            }
+        }
     }
 }
--- src/main/java/org/example/graph/KaMa98_matrix.java
+++ src/main/java/org/example/graph/KaMa98_matrix.java
@@ -7,55 +7,79 @@ import java.util.Scanner;
 public class KaMa98_matrix {
 
     static List<List<Integer>> result;
-    static List<Integer> list;
+    static List<Integer> path;
 
     public static void main(String[] args) {
 
         result = new ArrayList<>();
-        list = new ArrayList<>();
+        path = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
 
         // 邻接矩阵法
-        int[][] matrix = new int[n + 1][n + 1];
-
+        int[][] graph = new int[n + 1][n + 1];
+        // 读取输入，构建邻接矩阵
         for (int i = 0; i < m; i++) {
-            matrix[scanner.nextInt()][scanner.nextInt()] = 1;
+            graph[scanner.nextInt()][scanner.nextInt()] = 1;
         }
 
-        list.add(1);
-        dfs(matrix, 1, n);
+        // 开始dfs
+        path.add(1);
+        dfs(graph, 1, n);
 
-        if (result.size() == 0)
+        // 考虑特殊结果
+        if (result.size() == 0) {
             System.out.println(-1);
+        }
 
-        for (int i = 0; i < result.size(); i++) {
-            List<Integer> integers = result.get(i);
-            for (int j = 0; j < integers.size() - 1; j++) {
-                System.out.print(integers.get(j) + " ");
-            }
-            if (i != result.size() - 1) {
-                System.out.println(integers.get(integers.size() - 1));
-            } else {
-                System.out.print(integers.get(integers.size() - 1));
+//        for (int i = 0; i < result.size(); i++) {
+//            List<Integer> integers = result.get(i);
+//            for (int j = 0; j < integers.size() - 1; j++) {
+//                System.out.print(integers.get(j) + " ");
+//            }
+//            if (i != result.size() - 1) {
+//                System.out.println(integers.get(integers.size() - 1));
+//            } else {
+//                System.out.print(integers.get(integers.size() - 1));
+//            }
+//        }
+        // 打印结果
+        for (List<Integer> integers : result) {
+            for (int i = 0; i < integers.size(); i++) {
+                if (i == integers.size() - 1) {
+                    System.out.println(integers.get(i));
+                } else {
+                    System.out.print(integers.get(i) + " ");
+                }
             }
         }
     }
 
-    public static void dfs(int[][] matrix, int x, int n) {
-        for (int i = x + 1; i <= n; i++) {
-            if (i == n && matrix[x][i] == 1) {
-                list.add(i);
-                // result.add(list);
-                result.add(new ArrayList<>(list));
-                list.remove(list.size() - 1);
-            } else if (matrix[x][i] == 1) {
-                list.add(i);
-                dfs(matrix, i, n);
-                list.remove(list.size() - 1);
+    public static void dfs(int[][] graph, int x, int n) {
+        if (x == n) {
+            result.add(new ArrayList<>(path));
+        } else {
+            for (int i = x + 1; i < graph[x].length; i++) {
+                if (graph[x][i] == 1) {
+                    path.add(i);
+                    dfs(graph, i, n);
+                    path.remove(path.size() - 1);
+                }
             }
         }
+//        for (int i = x + 1; i <= n; i++) {
+//            if (i == n && graph[x][i] == 1) {
+//                list.add(i);
+//                // result.add(list);
+//                result.add(new ArrayList<>(list));
+//                list.remove(list.size() - 1);
+//            } else if (graph[x][i] == 1) {
+//                list.add(i);
+//                dfs(graph, i, n);
+//                list.remove(list.size() - 1);
+//            }
+//        }
     }
 }
