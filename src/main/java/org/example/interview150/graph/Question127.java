308ebaa795a3c411df8febce8e8a76a4725e5e05
lry <2920255877@qq.com>
2024-11-17 19:47:11 +0800

    2024/11/07 广度优先遍历（最短路径 还可以使用Dijkstra、Bellman Ford算法）

---
+++ src/main/java/org/example/interview150/graph/Question127.java
@@ -0,0 +1,55 @@
+package org.example.interview150.graph;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Question127 {
+    public static void main(String[] args) {
+        new Question127().ladderLength("hit", "cog", Arrays.asList("hot", "dot", "dog", "lot", "log", "cog"));
+    }
+
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        LinkedList<String> queue = new LinkedList<>();
+        queue.add(beginWord);
+
+        boolean[] visited = new boolean[wordList.size()];
+        Arrays.fill(visited, false);
+
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < wordList.size(); i++) {
+            map.put(wordList.get(i), i);
+        }
+
+        int res = 1;
+        int len = 1;
+        while (!queue.isEmpty()) {
+            String s = queue.removeFirst();
+            len--;
+
+            for (int i = 0; i < s.length(); i++) {
+                for (char c = 'a'; c <= 'z'; c++) {
+                    String s2 = s.substring(0, i) + c + s.substring(i + 1);
+                    if (map.containsKey(s2) && visited[map.get(s2)] == false) {
+                        queue.add(s2);
+                        visited[map.get(s2)] = true;
+                        if (s2.equals(endWord)) {
+                            res++;
+                            return res;
+                        }
+
+                    }
+                }
+
+            }
+
+            if (len == 0) {
+                res++;
+                len = queue.size();
+            }
+        }
+
+        return 0;
+    }
+}
