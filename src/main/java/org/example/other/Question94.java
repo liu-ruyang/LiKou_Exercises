918db3b50c6b52cad0053ba67f1c0d5a977d8942
liu-ruyang <2920255877@qq.com>
2024-12-05 13:19:53 +0800

    2024/12/05

--- src/main/java/org/example/other/Question94.java
+++ src/main/java/org/example/other/Question94.java
@@ -18,20 +18,16 @@ public class Question94 {
 
         while (!stack.isEmpty()) {
 
-            TreeNode node = stack.pop();    // 将该节点弹出，避免重复操作，下面再将右中左节点添加到栈中
+            TreeNode node = stack.pop();                           // 将该节点弹出，避免重复操作，下面再将右中左节点添加到栈中
 
             if (node != null) {
-                if (node.right != null) {   // 添加右节点(空节点不入栈)
-                    stack.push(node.right);
-                }
-                stack.push(node);           // 添加中节点
-                stack.push(null);        // 中节点访问过，但是还没有处理，加入空节点做为标记。//要处理的最终都是作为某一个树结构的中间节点位置进行处理的
-                if (node.left != null) {    // 添加左节点(空节点不入栈)
-                    stack.push(node.left);
-                }
-            } else {                // 只有遇到空节点的时候，才将下一个节点放进结果集 // 说明栈中下一个是将要被处理的元素
-                TreeNode node1 = stack.pop();   // 重新取出栈中元素
-                res.add(node1.val);             // 加入到结果集
+                if (node.right != null) stack.push(node.right);    // 添加右节点(空节点不入栈)
+                stack.push(node);                                  // 添加中节点
+                stack.push(null);                               // 中节点访问过，但是还没有处理，加入空节点做为标记。//要处理的最终都是作为某一个树结构的中间节点位置进行处理的
+                if (node.left != null) stack.push(node.left);      // 添加左节点(空节点不入栈)
+            } else {                                               // 只有遇到空节点的时候，才将下一个节点放进结果集 // 说明栈中下一个是将要被处理的元素
+                TreeNode node1 = stack.pop();                      // 重新取出栈中元素
+                res.add(node1.val);                                // 加入到结果集
             }
         }
 
